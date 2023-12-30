b2eb8a42b253edd004fffd88308d65bc0e67d543
MilaNeMila <57010255+MilaNeMila@users.noreply.github.com>
2023-12-30 18:20:20 +0400

    Update Main.java

--- src/main/java/mila/nemila/Main.java
+++ src/main/java/mila/nemila/Main.java
@@ -2,7 +2,8 @@ package mila.nemila;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(String testClass);
-    }
-
+        System.out.println(public void testClass{
+    return "дорогой дневник, мне не подобрать слов чтобы описать боль и унижение которые я испытал сегодня, моя жизнь поломана навсегда..";
+});
+}
 }
