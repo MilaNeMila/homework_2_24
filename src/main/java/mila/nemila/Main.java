4b71a7db05cb23476cd9efb91059420a7ff4f54c
MilaNeMila <lolipops313@gmail.com>
2023-12-30 18:19:09 +0400

    commit

--- src/main/java/mila/nemila/Main.java
+++ src/main/java/mila/nemila/Main.java
@@ -4,8 +4,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(testClass);
     }
-
-public void testClass{
-    return "дорогой дневник, мне не подобрать слов чтобы описать боль и унижение которые я испытал сегодня, моя жизнь поломана навсегда..";
-}
 }
