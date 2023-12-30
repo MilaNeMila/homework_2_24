90d5c03000b85740e233031c89b013f472405956
MilaNeMila <lolipops313@gmail.com>
2023-12-30 18:09:09 +0400

    commit

--- src/main/java/mila/nemila/Main.java
+++ src/main/java/mila/nemila/Main.java
@@ -1,12 +1,8 @@
 package mila.nemila;
 
-public String message;
 public class Main {
     public static void main(String[] args) {
         System.out.println(String testClass);
     }
 
-public void testClass{
-    return "hello qa.guru!";
-}
 }
