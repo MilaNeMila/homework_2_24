b5d3324b6424f26cfef4788baca5b3d7079031ac
MilaNeMila <lolipops313@gmail.com>
2023-12-30 18:17:10 +0400

    commit

--- src/main/java/mila/nemila/Main.java
+++ src/main/java/mila/nemila/Main.java
@@ -2,7 +2,10 @@ package mila.nemila;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(String testClassqweasdgwrweczdacs);
+        System.out.println(testClass);
     }
 
+public void testClass{
+    return "дорогой дневник, мне не подобрать слов чтобы описать боль и унижение которые я испытал сегодня, моя жизнь поломана навсегда..";
+}
 }
